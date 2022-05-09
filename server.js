467e183fde1f48b30fb75ffa00cbcd7b2a72c790
Nigel Fijnheer <neinno@re-boyds-145-109-156-161.wireless.hva.nl>
2022-05-09 11:37:59 +0200

    updates views

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ app.listen(port, () => {
 })
 
 
+/* Middleware */
 app.use(express.static('public'));
-
+app.set('view engine', 'pug');
 
 /* Routes */
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/index', (req, res) => {
+  res.render('index')
 })
