870651d6707e0c9289ceee03681c9fee92e99ce3
Nigel Fijnheer <neinno@MacBook-Pro-van-Nigel.local>
2022-05-14 16:40:57 +0200

    updated server.js, started using template handlebars

--- server.js
+++ server.js
@@ -9,13 +9,42 @@ app.listen(port, () => {
 
 /* Middleware */
 app.use(express.static('public'));
-app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+/* Handlebars */
+const { engine } = require("express-handlebars");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
 
 /* Routes */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/index', (req, res) => {
   res.render('index')
 })
+
+app.get('/add', (req, res) => {
+  res.render('add', {data: data});
+})
+
+app.post('/add', (req, res) => {
+  console.log(req.body);
+
+  data.push({
+    merk: req.body.merk,
+    type: req.body.type,
+    kleur: req.body.kleur
+  })
+
+  res.render('add', { data: data });
+})
+
+const data = [
+  {
+    merk: 'audi',
+    type: 'a4',
+    kleur: 'zwart'
+  }
+];
