67ffbacbca259d2a42c70dbdde9655baaeac38b8
Nigel Fijnheer <neinno@re-boyds-145-109-140-117.wireless.hva.nl>
2022-05-16 09:39:19 +0200

    added gulp, updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 
 /* Middleware */
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 /* Handlebars */
 const { engine } = require("express-handlebars");
@@ -21,34 +21,34 @@ app.set("views", "./views");
 
 
 /* Routes */
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/add', (req, res) => {
-  res.render('add', {data: data});
-})
+app.get("/add", (req, res) => {
+  res.render("add", {data: data});
+});
 
-app.get('/profile', (req, res) => {
-  res.render('profile');
-})
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
 
-app.post('/add', (req, res) => {
+app.post("/add", (req, res) => {
   console.log(req.body);
 
   data.push({
     merk: req.body.merk,
     type: req.body.type,
     kleur: req.body.kleur
-  })
+  });
 
-  res.render('add', { data: data });
-})
+  res.render("add", { data: data });
+});
 
 const data = [
   {
-    merk: 'audi',
-    type: 'a4',
-    kleur: 'zwart'
+    merk: "audi",
+    type: "a4",
+    kleur: "zwart"
   }
 ];
