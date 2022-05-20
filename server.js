52af4dbca6121fa1316a5f214e629c9917c679a8
Nigel Fijnheer <neinno@re-boyds-145-109-142-180.wireless.hva.nl>
2022-05-20 12:39:15 +0200

    added database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000; 
+const dotenv = require("dotenv").config();
+const { MongoClient } = require ("mongodb");
+const { ObjectId } = require ("mongodb");
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
@@ -9,9 +12,9 @@ app.listen(port, () => {
 
 /* Middleware */
 app.use(express.static("public"));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 
 /* Handlebars */
 const { engine } = require("express-handlebars");
@@ -21,7 +24,7 @@ app.set("views", "./views");
 
 
 /* Routes */
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index");
 });
 
@@ -32,6 +35,13 @@ app.get("/add", (req, res) => {
 app.get("/profile", (req, res) => {
   res.render("profile");
 });
+
+// app.get("/match", (req, res) => {
+//   const cars = await db.collection("cars").find({},{}).toArray();
+//   const merk = (cars.length == 0) ?"no cars found" : "cars";
+
+//   res.render("cars", {merk});
+// });
 
 app.post("/add", (req, res) => {
   console.log(req.body);
@@ -52,3 +62,19 @@ const data = [
     kleur: "zwart"
   }
 ];
+
+async function connectDB() {
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const cars = database.collection("cars");
+  try { 
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+  } catch (error) {
+    throw error;
+  }
+}
+
